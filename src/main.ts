ca4c0c0ab43e0bb275a6e64f9fafbaac747c35eb
Cameron Presley <Cameron@TheSoftwareMentor.com>
2024-11-14 21:15:22 -0500

    moving to env variables

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
-const message: string = "Hello World!";
+import { getOnCallInformation } from "./integrations/pagerduty.ts";
 
-console.log(message);
+const escalationPolicy = Deno.env.get("ESCALATION_POLICY_ID")!;
+const onCallInfo = await getOnCallInformation(escalationPolicy);
+
+console.log(`${onCallInfo.name} <${onCallInfo.email}> is currently on call!`);
+
+
+
